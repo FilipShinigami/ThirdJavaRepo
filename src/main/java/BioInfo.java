4c2a29b48e5437fef9462c10f6e9cd51696c8d2e
Filip Koceski <filip.koceski2001@gmail.com>
2024-03-04 11:47:03 +0100

    Add ThirdJavaRepo

---
+++ src/main/java/BioInfo.java
@@ -0,0 +1,28 @@
+
+public class BioInfo {
+
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		String Name = "Filip"; 
+		
+		
+		int Age = 22; // za godini ni e potreben integer
+			
+		float hight = 173;
+			// za dacimali ni e potreben double
+		double weight = 54.6; 
+			
+			// za karakteri koristime char
+		char BloodType = '0'; 
+		
+		boolean BloodType2 = true; //Positive
+		
+			// so ova programata ke ni gi dade potrebnite informacii na ekran
+		System.out.println(Name +" "+Age+" "+ hight +" "+weight+" "+ BloodType+ ""+ BloodType2);  
+
+		
+	}
+
+}
